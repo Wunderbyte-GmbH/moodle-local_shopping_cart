eccb52baa93498d39089daafdf22dd8c6fb4c7c0
georgmaisser <georg.maisser@wunderbyte.at>
2022-01-13 21:40:52 +0100

    Add first behat test. Not working, because cache is not working in behat environment. needs to be fixed.

---
+++ tests/behat/behat_shopping_cart.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Behat question-related steps definitions.
+ *
+ * @package    local_shopping_cart
+ * @category   test
+ * @copyright  2022 Wunderbyte Gmbh <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use local_shopping_cart\local\entities\cartitem;
+use local_shopping_cart\shopping_cart;
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+/**
+ * Steps definitions related with the mooduell table management.
+ *
+ * @package    local_shopping_cart
+ * @category   test
+ * @copyright  2022 Wunderbyte Gmbh <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_shopping_cart extends behat_base
+{
+
+    /**
+     * Put item in my cart.
+     * This ads a dummy item to the cache. After reloading the page, the item will be visible.
+     * @Given /^I put "(?P<itemname_string>(?:[^"]|\\")*)" in my cart$/
+     * @return void
+     */
+    public function i_put_item_in_my_cart(string $itemname) {
+        global $DB;
+
+        $item = new cartitem(1, $itemname, 10, 'EUR', 'mod_quiz', 'item description');
+        $data = $item->getitem();
+
+        $shoppingcart = new shopping_cart();
+        $shoppingcart->add_item_to_cart($data);
+    }
+}
---
+++ tests/behat/shopping_cart_add.feature
@@ -0,0 +1,35 @@
+@local @local_shopping_cart @javascript
+
+Feature: Test purchase process in shopping cart.
+  In order to buy an item
+  As a student
+  I need to put an item in my cart and proceed to checkout
+
+  Background:
+    Given the following "users" exist:
+      | username | firstname | lastname |
+      | user1    | Username  | 1        |
+      | user2    | Username  | 2        |
+      | teacher  | Teacher   | 3        |
+      | manager  | Manager   | 4        |
+    And the following "courses" exist:
+      | fullname | shortname |
+      | Course 1 | C1        |
+    And the following "course enrolments" exist:
+      | user     | course | role           |
+      | user1    | C1     | student        |
+      | user2    | C1     | student        |
+      | teacher  | C1     | editingteacher |
+
+  @javascript
+  Scenario: Add an item to the shopping cart
+    Given I log in as "user1"
+    When I am on "Course 1" course homepage
+    And I put "my test item" in my cart
+    And I wait "1" seconds
+    And I reload the page
+    And I click on "#nav-shopping_cart-popover-container" "css_element"
+    And I wait "5" seconds
+    And I reload the page
+    And I wait "5" seconds
+    Then I should see "my test item"
