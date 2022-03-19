23bcd4549a810860a8d0b155bdcc208fcc2a39a4
georgmaisser <georg.maisser@wunderbyte.at>
2022-03-19 23:15:28 +0100

    version 0.1.0 (2022031900)

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
 $plugin->release = '0.1.0';
-$plugin->version = 2022021611;
+$plugin->version = 2022031900;
 $plugin->requires = 2020051700;
 $plugin->maturity = MATURITY_ALPHA;
