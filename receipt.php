c09a07f989a0813c009b8c118222ec7160b9879b
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-07-15 10:31:34 +0200

    Linting.

--- receipt.php
+++ receipt.php
@@ -31,9 +31,9 @@ require_login();
 $id = required_param('id', PARAM_INT);
 $userid = required_param('userid', PARAM_INT);
 
-// create new PDF document
+// Create new PDF document.
 $pdf = new TCPDF('p', 'pt', 'A4', true, 'UTF-8', false);
-// Set some content to print
+// Set some content to print.
 $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_url('/test.php');
@@ -101,30 +101,30 @@ $html = '
     }
 </style>
 '. $prehtml[0] . $itemhtml . $posthtml;
-// Print text using writeHTMLCell()
+// Print text using writeHTMLCell().
 
 
-// set document information
+// Set document information.
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Nicola Asuni');
 $pdf->SetTitle('TCPDF Example 001');
 $pdf->SetSubject('');
 $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
-// set header and footer fonts
+// Set header and footer fonts.
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-// set default monospaced font
+// Set default monospaced font.
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-// set auto page breaks
-$pdf->SetAutoPageBreak(FALSE, PDF_MARGIN_BOTTOM);
+// Set auto page breaks.
+$pdf->SetAutoPageBreak(false, PDF_MARGIN_BOTTOM);
 
-// set image scale factor
+// Set image scale factor.
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-// set some language-dependent strings (optional)
+// Set some language-dependent strings (optional).
 if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
     require_once(dirname(__FILE__).'/lang/eng.php');
     $pdf->setLanguageArray($l);
@@ -132,14 +132,14 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 
 // ---------------------------------------------------------
 
-// set default font subsetting mode
+// Set default font subsetting mode.
 $pdf->setFontSubsetting(true);
 
 $pdf->SetHeaderMargin(0);
 $pdf->SetFooterMargin(0);
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-// remove default footer
+// Remove default footer.
 $pdf->setPrintHeader(false);
 
 $pdf->setPrintFooter(false);
@@ -148,6 +148,6 @@ $pdf->AddPage();
 $pdf->Image($url->out(false), 0, 0, $pdf->getPageWidth(), $pdf->getPageHeight(), '', '', '', true, 300, '', false, false, 0);
 $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
 
-// Close and output PDF document
+// Close and output PDF document.
 // This method has several options, check the source code documentation for more information.
 $pdf->Output($userdetails->firstname . '_' . $userdetails->lastname . '_' . $date . '.pdf', 'I');
