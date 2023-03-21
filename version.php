483411b875b866f8653c4aceca5c593703380479
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-03-21 14:28:01 +0100

    Version 0.4.1 (2023032100)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
-$plugin->release = '0.4.0';
-$plugin->version = 2023032000;
+$plugin->release = '0.4.1';
+$plugin->version = 2023032100;
 $plugin->requires = 2020051700;
 $plugin->maturity = MATURITY_ALPHA;
