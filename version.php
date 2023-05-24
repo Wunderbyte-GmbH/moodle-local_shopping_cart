cf3eb984d96ef8ebd8217ac9cc11d4fa699ecacd
Georg Maißer <georg.maisser@wunderbyte.at>
2023-05-24 14:07:25 +0200

    Version 0.4.5 (2023052400)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
-$plugin->release = '0.4.4';
-$plugin->version = 2023052202;
+$plugin->release = '0.4.5';
+$plugin->version = 2023052400;
 $plugin->requires = 2020051700;
 $plugin->maturity = MATURITY_ALPHA;
