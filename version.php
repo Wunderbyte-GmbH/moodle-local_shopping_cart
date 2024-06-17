0a4e95cefbadf3c0d89c26895c7dc67aaa602601
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-06-17 10:51:23 +0200

    Version (2024061202)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
 $plugin->release = '0.9.6';
-$plugin->version = 2024061201;
+$plugin->version = 2024061202;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
