76be339de68a4c5c77917b28d71fc6a437f41163
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-07-18 11:40:05 +0200

    fix version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
-$plugin->release = '0.9.8';
-$plugin->version = 2024070401;
+$plugin->release = '0.9.9';
+$plugin->version = 2024071800;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 404];
