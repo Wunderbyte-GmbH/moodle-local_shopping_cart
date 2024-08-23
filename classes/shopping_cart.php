dead01ab8889993d5a65fe2cb27b0740effebc6b
Andrii Semenets <semteacher@gmail.com>
2024-08-23 19:42:26 +0300

    fix error when $data had been set to empty []

--- classes/shopping_cart.php
+++ classes/shopping_cart.php
@@ -964,7 +964,7 @@ class shopping_cart {
             // We now trigger an event to card & cashier checkout to react on it.
             if (!empty($identifier)) {
                 $schlist = new shoppingcart_history_list($userid, $identifier, true);
-                $data = [];
+
                 $schlist->insert_list($data);
                 $data['success'] = 1;
                 $data['finished'] = 1;
