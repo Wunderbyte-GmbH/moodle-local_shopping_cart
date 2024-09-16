0b07cbdcc9298193f8a1298f6057ea204c1caa62
Georg Maißer <georg.maisser@wunderbyte.at>
2024-09-16 10:44:01 +0200

    Version 0.9.23 (2024091600)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
-$plugin->release = '0.9.22';
-$plugin->version = 2024091300;
+$plugin->release = '0.9.23';
+$plugin->version = 2024091600;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 404];
