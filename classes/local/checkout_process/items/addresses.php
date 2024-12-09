38d6f0733999732f52d95603e7c78d9e88109ebd
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-12-09 16:26:02 +0100

    Improvement: New item setup

---
+++ classes/local/checkout_process/items/addresses.php
@@ -0,0 +1,139 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The cartstore class handles the in and out of the cache.
+ *
+ * @package local_shopping_cart
+ * @author Jacob Viertel
+ * @copyright 2024 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_shopping_cart\local\checkout_process\items;
+
+use local_shopping_cart\local\checkout_process\checkout_base_item;
+
+/**
+ * Class checkout
+ *
+ * @author Jacob Viertel
+ * @copyright 2024 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class addresses extends checkout_base_item {
+    /**
+     * Renders checkout item.
+     * @return bool
+     */
+    public static function is_active() {
+        if (get_config('local_shopping_cart', 'addresses_required')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Renders checkout item.
+     * @return string
+     */
+    public static function get_icon_progress_bar() {
+        return 'fa-solid fa-address-book';
+    }
+
+    /**
+     * Renders checkout item.
+     * @return array
+     */
+    public static function render_body() {
+        global $PAGE;
+        $data = self::get_template_render_data();
+        $template = $PAGE->get_renderer('local_shopping_cart')
+            ->render_from_template("local_shopping_cart/address", $data);
+
+        return [
+            'template' => $template,
+        ];
+    }
+
+    /**
+     * Generates the data for rendering the templates/address.mustache template.
+     *
+     * @return array all required template data
+     */
+    public static function get_template_render_data(): array {
+        global $USER, $DB;
+        $userid = $USER->id;
+        $data["usermail"] = $USER->email;
+        $data["username"] = $USER->firstname . $USER->lastname;
+        $data["userid"] = $userid;
+
+        // Get saved addresses for current user.
+        $sql = "SELECT *
+                FROM {local_shopping_cart_address}
+                WHERE userid=:userid
+                ORDER BY id DESC";
+
+        $params = ['userid' => $userid];
+        $addressesfromdb = $DB->get_records_sql($sql, $params);
+
+        $countries = get_string_manager()->get_list_of_countries();
+        $savedaddresses = [];
+        foreach ($addressesfromdb as $dbaddress) {
+            $dbaddress->country = $countries[$dbaddress->state];
+            $savedaddresses[] = $dbaddress;
+        }
+        $data['saved_addresses'] = $savedaddresses;
+
+        $requiredaddresseslocalized = self::get_required_address_data();
+        $data['required_addresses'] = array_values($requiredaddresseslocalized);
+        $data['required_addresses_keys'] = array_reduce($requiredaddresseslocalized, function ($keys, $addressdata) {
+            $keys[] = $addressdata['addresskey'];
+            return $keys;
+        }, []);
+        $data['required_addresses_multiple'] = count($requiredaddresseslocalized) > 1;
+        return $data;
+    }
+
+    /**
+     * Generates complete required-address data as specified by the plugin config.
+     *
+     * @return array list of all required addresses with a key and localized string
+     */
+    public static function get_required_address_data(): array {
+        $requiredaddresseslocalized = [];
+        $requiredaddresskeys = self::get_required_address_keys();
+        // Insert localized string for required address types.
+        foreach ($requiredaddresskeys as $addresstype) {
+            $requiredaddresseslocalized[$addresstype] = [
+                    "addresskey" => $addresstype,
+                    "addresslabel" => get_string('addresses:' . $addresstype, 'local_shopping_cart'),
+            ];
+        }
+        return $requiredaddresseslocalized;
+    }
+
+    /**
+     * Returns the required-address keys as specified in the plugin config.
+     *
+     * @return array list of all required address keys
+     */
+    public static function get_required_address_keys(): array {
+        $addressesrequired = get_config('local_shopping_cart', 'addresses_required');
+        $requiredaddresskeys = array_filter(explode(',', $addressesrequired));
+        return $requiredaddresskeys;
+    }
+}
