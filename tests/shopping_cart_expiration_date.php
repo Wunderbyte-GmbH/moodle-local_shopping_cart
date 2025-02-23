6b7d0689d72001f9e405aef40820e679f54c08b5
Andrii Semenets <semteacher@gmail.com>
2025-02-23 16:46:50 +0200

    refactoring to use external tool_mocktesttime plugin

--- tests/shopping_cart_expiration_date.php
+++ tests/shopping_cart_expiration_date.php
@@ -31,6 +31,7 @@ use local_shopping_cart\payment\service_provider;
 use local_shopping_cart\shopping_cart;
 use local_shopping_cart\local\cartstore;
 use local_shopping_cart\output\shoppingcart_history_list;
+use tool_mocktesttime\time_mock;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use stdClass;
@@ -38,8 +39,6 @@ use stdClass;
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
-
-require_once($CFG->dirroot . '/local/shopping_cart/tests/mocktime/time_mock.php');
 
 /**
  * Testing checkout in payment gateway local_shopping_cart
@@ -81,95 +80,7 @@ final class shopping_cart_expiration_date extends \advanced_testcase {
 
         $accountgateway1 = \core_payment\helper::save_payment_gateway($record);
 
-
-        $namespaces = [];
-        $directory = dirname(__DIR__, 3);
-
-        // Make sure the directory exists and is a directory
-        if (!is_dir($directory)) {
-            throw new Exception("Provided path is not a valid directory: $directory");
-        }
-
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
-
-        foreach ($files as $file) {
-            if ($file->getExtension() !== 'php') continue;
-
-            $contents = file_get_contents($file->getRealPath());
-            if (preg_match('/namespace\s+([a-zA-Z0-9_\\\\]+);/', $contents, $matches)) {
-                $namespace = trim($matches[1]);
-                if (!in_array($namespace, $namespaces)) {
-                    $namespaces[] = $namespace;
-                }
-            }
-        }
-
-        $overrideDir = __DIR__ . '/time_overrides/';
-        if (!is_dir($overrideDir)) {
-            mkdir($overrideDir, 0777, true);
-        }
-
-        foreach ($namespaces as $namespace) {
-
-            $namespace = str_replace('\\\\', '\\', $namespace);
-            $filename = $overrideDir . str_replace('\\', '_', $namespace) . '_time.php';
-
-            if (file_exists($filename)) {
-                continue;
-            }
-            $overrideCode = "<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * phpUnit cartitem_test class definitions.
- *
- * @package    local_shopping_cart
- * @category   test
- * @copyright  2025 Wunderbyte Gmbh <info@wunderbyte.at>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-namespace $namespace;
-
-/**
- * This function will oveerride my namespace
- *
- * @return [type]
- *
- */
-function time() {
-    return \local_shopping_cart\\time_mock::get_mock_time() ?? 1000000000;
-}
-";
-
-            file_put_contents($filename, $overrideCode);
-        }
-
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($overrideDir));
-
-        foreach ($files as $file) {
-            if ($file->getExtension() !== 'php') continue;
-
-            $path = $file->getRealPath();
-            if (strpos($path, 'local_shopping_cart_local_time') > 0) {
-                $a = 1;
-            }
-
-            require($path);
-        }
+        time_mock::init();
     }
 
 
