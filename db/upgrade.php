33f6c71d07acf98cc134359c6f3d2cfdbcdd603f
Georg Maißer <georg.maisser@wunderbyte.at>
2025-05-20 10:50:42 +0200

    Improvement: Change vat number to precision 24

--- db/upgrade.php
+++ db/upgrade.php
@@ -747,6 +747,18 @@ function xmldb_local_shopping_cart_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2025051900, 'local', 'shopping_cart');
     }
 
+    if ($oldversion < 2025052000) {
+        // Changing precision of field vatnumber on table local_shopping_cart_history to (24).
+        $table = new xmldb_table('local_shopping_cart_history');
+        $field = new xmldb_field('vatnumber', XMLDB_TYPE_CHAR, '24', null, null, null, null, 'address_shipping');
+
+        // Launch change of precision for field vatnumber.
+        $dbman->change_field_precision($table, $field);
+
+        // Shopping_cart savepoint reached.
+        upgrade_plugin_savepoint(true, 2025052000, 'local', 'shopping_cart');
+    }
+
     // For further information please read {@link https://docs.moodle.org/dev/Upgrade_API}.
     //
     // You will also have to create the db/install.xml file by using the XMLDB Editor.
