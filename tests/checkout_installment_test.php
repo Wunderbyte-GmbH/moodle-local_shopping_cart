924ed01c95dcdb7a4fc8d564a5b64681005a8548
Andrii Semenets <semteacher@gmail.com>
2025-08-15 21:43:48 +0300

    phpunit: test on installment (incomplete)(#67)

---
+++ tests/checkout_installment_test.php
@@ -0,0 +1,263 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Testing checkout wiht installment
+ *
+ * @package    local_shopping_cart
+ * @category   test
+ * @copyright  2024 Wunderbyte Gmbh <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_shopping_cart;
+
+use local_shopping_cart\local\checkout_process\checkout_manager;
+use local_shopping_cart\payment\service_provider;
+use local_shopping_cart\shopping_cart;
+use local_shopping_cart\local\cartstore;
+use paygw_payone\external\get_config_for_js;
+use local_shopping_cart\external\get_price;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/payment/gateway/payone/thirdparty/vendor/autoload.php');
+require_once($CFG->dirroot . '/local/shopping_cart/tests/checkout_process_test_setup.php');
+
+/**
+ * Testing checkout wiht installment
+ *
+ * @package    local_shopping_cart
+ * @category   test
+ * @copyright  2024 Wunderbyte Gmbh <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @runTestsInSeparateProcesses
+ */
+final class checkout_installment_test extends \local_shopping_cart\checkout_process_test_setup {
+    /**
+     * Test transaction complete process
+     *
+     * @dataProvider checkoutprocessdataprovider
+     * @param array $config Config settings for the test
+     * @param array $changedinputsteps JSON input for the checkout process
+     * @param array $assertions Assertion function for the test
+     * @covers \local_shopping_cart\local\checkout_process\checkout_manager
+     */
+    public function test_checkout_process(array $config, array $changedinputsteps, array $assertions): void {
+        global $DB;
+
+        // Create users.
+        $student1 = $this->getDataGenerator()->create_user();
+        $this->setUser($student1);
+
+        // Validate payment account if it has a config.
+        $record1 = $DB->get_record('payment_accounts', ['id' => $this->account->get('id')]);
+        $this->assertEquals('PayOne1', $record1->name);
+        $this->assertCount(1, $DB->get_records('payment_gateways', ['accountid' => $this->account->get('id')]));
+
+        // Set local_shopping_cart to use the payment account.
+        set_config('accountid', $this->account->get('id'), 'local_shopping_cart');
+
+        // Add the only item with installment to the cart.
+        shopping_cart::add_item_to_cart(
+            'local_shopping_cart',
+            'main',
+            5,
+            $student1->id
+        );
+
+        $cartstore = cartstore::instance($student1->id);
+        $data = $cartstore->get_localized_data();
+        $cartstore->get_expanded_checkout_data($data);
+        $cartitems1 = $cartstore->get_items();
+
+        foreach ($config as $key => $value) {
+            set_config($key, $value, 'local_shopping_cart');
+        }
+
+        $cartitems2 = $cartstore->get_items();
+
+        $addresids = $this->generate_fake_addresses($student1);
+
+        $managercache = [];
+        foreach ($changedinputsteps as $step => &$stepdata) {
+            while (str_contains($stepdata['changedinput'], 'REPLACE_WITH_ADDRESSID')) {
+                $replacement = array_shift($addresids);
+                $replacecount = 1;
+                $stepdata['changedinput'] = str_replace(
+                    'REPLACE_WITH_ADDRESSID',
+                    $replacement,
+                    $stepdata['changedinput'],
+                    $replacecount
+                );
+            }
+            $checkoutmanager = new checkout_manager($data, $stepdata['controlparameter']);
+            $checkoutmanagerrenderedoverview = $checkoutmanager->render_overview();
+            $managercache = $checkoutmanager->check_preprocess($stepdata['changedinput']);
+
+            $balance = shopping_cart_credits::add_credit($student1->id, $stepdata['generatedcredits'], 'EUR', '');
+            shopping_cart::save_used_credit_state($student1->id, $stepdata['usecredit']);
+
+            //$prices = get_price::execute($student1->id, $stepdata['usecredit'], $stepdata['useinstallments']);
+            $cartstore->save_useinstallments_state($stepdata['useinstallments']);
+
+            // The price is calculated from the cache, but there is a fallback to DB, if no cache is available.
+            $cartstore = cartstore::instance($student1->id);
+            //$data = $cartstore->get_data();
+            $data = $cartstore->get_localized_data();
+            $cartstore->get_expanded_checkout_data($data);
+
+            service_provider::get_payable('', $data['identifier']);
+            if ($stepdata['purgecache']) {
+                \cache_helper::purge_all();
+            }
+        }
+        unset($stepdata);
+
+        $cartitems3 = $cartstore->get_items();
+
+        //service_provider::get_payable('', $data['identifier']);
+        service_provider::deliver_order('', $data['identifier'], 1, $student1->id);
+
+        $res = get_config_for_js::execute('local_shopping_cart', 'main', $data['identifier']);
+        $historyrecords = $DB->get_records('local_shopping_cart_history');
+
+        foreach ($assertions as $step => $assertion) {
+            if ($step === 'assertcartstoreexacttax') {
+                $this->assertcartstoreexacttax($managercache, $historyrecords, $assertion);
+            } else {
+                $this->$assertion($managercache, $historyrecords);
+            }
+        }
+    }
+
+    /**
+     * Data provider for checkout process tests.
+     *
+     * @return array[]
+     */
+    public static function checkoutprocessdataprovider(): array {
+        return [
+            'User has cerdits, uses credits, cache cleared' => [
+                [
+                    'addresses_required' => 'shipping',
+                    'taxcategories' => 'default A:20 B:20 C:10
+                        AT A:20 B:10 C:0
+                        DE A:19 B:10 C:0',
+                    'enabletax' => '1',
+                    'defaultcostcenterforcredits' => 'IT140030',
+                    'costcenterstrings' => 'IT140030',
+                    'enableinstallments' => '1',
+                    'timebetweenpayments' => '2',
+                    'reminderdaysbefore' => '1',
+                ],
+                [
+                    [
+                        'changedinput' => '[{"name":"selectedaddress_shipping","value":"REPLACE_WITH_ADDRESSID"}]',
+                        'controlparameter' => [
+                            "currentstep" => 0,
+                            "action" => null,
+                        ],
+                        'generatedcredits' => '5',
+                        'usecredit' => true,
+                        'useinstallments' => true,
+                        'purgecache' => true,
+                    ],
+                ],
+                [
+                    'assertbalanceisnotnull',
+                    'payedpriceissame',
+                    'assertcartstoretax',
+                    'assertcartstoreexacttax' => [
+                        [
+                            'itemid' => "5",
+                            'price' => "42.42",
+                            'tax' => "7.070",
+                            'taxpercentage' => "0.2000",
+                            'taxcategory' => "B",
+                            'usecredit' => 1,
+                        ],
+                    ],
+                ],
+            ],
+            'User has cerdits, and uses credits, cache cleared' => [
+                [
+                    'taxcategories' => 'default A:20 B:20 C:10
+                        AT A:20 B:10 C:0
+                        DE A:19 B:10 C:0',
+                    'enabletax' => '1',
+                    'defaultcostcenterforcredits' => 'IT140030',
+                    'costcenterstrings' => 'IT140030',
+                ],
+                [
+                    [
+                        'generatedcredits' => '5',
+                        'usecredit' => true,
+                        'purgecache' => true,
+                    ],
+                ],
+                [
+                    'assertbalanceisnotnull',
+                    'payedpriceissame',
+                ],
+            ],
+            'User has cerdits, but does not uses credits, cache not cleared' => [
+                [
+                    'taxcategories' => 'default A:20 B:20 C:10
+                        AT A:20 B:10 C:0
+                        DE A:19 B:10 C:0',
+                    'enabletax' => '1',
+                    'defaultcostcenterforcredits' => 'IT140030',
+                    'costcenterstrings' => 'IT140030',
+                ],
+                [
+                    [
+                        'generatedcredits' => '5',
+                        'usecredit' => false,
+                        'purgecache' => false,
+                    ],
+                ],
+                [
+                    'assertbalanceisnotnull',
+                    'payedpriceissame',
+                ],
+            ],
+            'User has cerdits, and uses credits, cache not cleared' => [
+                [
+                    'taxcategories' => 'default A:20 B:20 C:10
+                        AT A:20 B:10 C:0
+                        DE A:19 B:10 C:0',
+                    'enabletax' => '1',
+                    'defaultcostcenterforcredits' => 'IT140030',
+                    'costcenterstrings' => 'IT140030',
+                ],
+                [
+                    [
+                        'generatedcredits' => '5',
+                        'usecredit' => true,
+                        'purgecache' => false,
+                    ],
+                ],
+                [
+                    'assertbalanceisnotnull',
+                    'payedpriceissame',
+                ],
+            ],
+        ];
+    }
+}
