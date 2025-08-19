b2e8a6c397ce165751284be0b9b832af39c7ac85
Georg Maißer <georg.maisser@wunderbyte.at>
2025-08-19 12:47:22 +0200

    Linting

--- classes/external/get_shopping_cart_items.php
+++ classes/external/get_shopping_cart_items.php
@@ -48,7 +48,6 @@ require_once($CFG->libdir . '/externallib.php');
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class get_shopping_cart_items extends external_api {
-
     /**
      * Describes the parameters for this service.
      *
@@ -100,77 +99,95 @@ class get_shopping_cart_items extends external_api {
      * @return external_single_structure
      */
     public static function execute_returns(): external_single_structure {
-
         return new external_single_structure(
-                [
-                        'count' => new external_value(PARAM_INT, 'Number of items', VALUE_REQUIRED),
-                        'price' => new external_value(PARAM_FLOAT, 'Total price', VALUE_REQUIRED),
-                        'price_net' => new external_value(PARAM_FLOAT, 'Total net price', VALUE_DEFAULT, 0),
-                        'credit' => new external_value(PARAM_FLOAT, 'Credit', VALUE_REQUIRED),
-                        'currency' => new external_value(PARAM_ALPHA, 'Currency', VALUE_REQUIRED),
-                        'taxesenabled' => new external_value(PARAM_BOOL, 'Is tax information enabled', VALUE_REQUIRED),
-                        'initialtotal' => new external_value(PARAM_FLOAT, 'Initial price before deduced credits', VALUE_REQUIRED),
-                        'initialtotal_net' => new external_value(
-                            PARAM_FLOAT,
-                            'Initial price before deduced credits net amount',
-                            VALUE_DEFAULT, 0),
-                        'remainingcredit' => new external_value(PARAM_FLOAT, 'Credits after reducation', VALUE_REQUIRED),
-                        'deductible' => new external_value(PARAM_FLOAT, 'Deductible amount', VALUE_REQUIRED),
-                        'usecredit' => new external_value(PARAM_INT, 'If we want to use the credit or not'),
-                        'discount' => new external_value(PARAM_FLOAT, 'The sum of all discounts on the items.', VALUE_DEFAULT, 0),
-                        'expirationtime' => new external_value(PARAM_INT, 'Expiration timestamp of cart', VALUE_REQUIRED),
-                        'nowdate' => new external_value(PARAM_INT, 'current Timestamp', VALUE_REQUIRED),
-                        'maxitems' => new external_value(PARAM_INT, 'Max Items', VALUE_REQUIRED),
-                        'items' => new external_multiple_structure (
-                                new external_single_structure(
-                                        [
-                                                'userid' => new external_value(PARAM_INT, 'userid', VALUE_OPTIONAL),
-                                                'itemid' => new external_value(PARAM_INT, 'Item id', VALUE_OPTIONAL),
-                                                'itemname' => new external_value(PARAM_TEXT, 'Item name', VALUE_OPTIONAL),
-                                                'price' => new external_value(PARAM_FLOAT, 'Price of item', VALUE_OPTIONAL),
-                                                'price_gross' => new external_value(
-                                                    PARAM_FLOAT,
-                                                    'Gross price of item',
-                                                    VALUE_OPTIONAL),
-                                                'price_net' => new external_value(
-                                                    PARAM_FLOAT,
-                                                    'Net price of item',
-                                                    VALUE_OPTIONAL),
-                                                'tax' => new external_value(
-                                                    PARAM_FLOAT,
-                                                    'Net tax of item price',
-                                                    VALUE_OPTIONAL),
-                                                'taxcategory' => new external_value(
-                                                    PARAM_TAG,
-                                                    'Tax category of item',
-                                                    VALUE_OPTIONAL),
-                                                'taxpercentage' => new external_value(
-                                                    PARAM_FLOAT,
-                                                    'Tax percentage of item price as float',
-                                                    VALUE_OPTIONAL),
-                                                'taxpercentage_visual' => new external_value(PARAM_FLOAT,
-                                                        'Tax percentage of item price as an int', VALUE_OPTIONAL),
-                                                'currency' => new external_value(PARAM_ALPHA, 'Currency', VALUE_OPTIONAL),
-                                                'componentname' => new external_value(
-                                                    PARAM_COMPONENT,
-                                                    'Component name',
-                                                    VALUE_OPTIONAL),
-                                                'costcenter' => new external_value(
-                                                    PARAM_TEXT,
-                                                    'Cost center for item',
-                                                    VALUE_OPTIONAL),
-                                                'area' => new external_value(PARAM_TEXT, 'Area', VALUE_OPTIONAL),
-                                                'description' => new external_value(PARAM_RAW, 'Item description', VALUE_OPTIONAL),
-                                                'imageurl' => new external_value(PARAM_RAW, 'Image url', VALUE_OPTIONAL),
-                                                'canceluntil' => new external_value(PARAM_INT,
-                                                        'Timestamp until when cancel is possible', VALUE_OPTIONAL),
-                                                'nodelete' => new external_value(PARAM_INT, 'Marker for no delete', VALUE_OPTIONAL),
-                                                'itemamount' => new external_value(PARAM_INT, 'Number of one item', VALUE_OPTIONAL),
+            [
+                'count' => new external_value(PARAM_INT, 'Number of items', VALUE_REQUIRED),
+                'price' => new external_value(PARAM_FLOAT, 'Total price', VALUE_REQUIRED),
+                'price_net' => new external_value(PARAM_FLOAT, 'Total net price', VALUE_DEFAULT, 0),
+                'credit' => new external_value(PARAM_FLOAT, 'Credit', VALUE_REQUIRED),
+                'currency' => new external_value(PARAM_ALPHA, 'Currency', VALUE_REQUIRED),
+                'taxesenabled' => new external_value(PARAM_BOOL, 'Is tax information enabled', VALUE_REQUIRED),
+                'initialtotal' => new external_value(PARAM_FLOAT, 'Initial price before deduced credits', VALUE_REQUIRED),
+                'initialtotal_net' => new external_value(
+                    PARAM_FLOAT,
+                    'Initial price before deduced credits net amount',
+                    VALUE_DEFAULT,
+                    0
+                ),
+                'remainingcredit' => new external_value(PARAM_FLOAT, 'Credits after reducation', VALUE_REQUIRED),
+                'deductible' => new external_value(PARAM_FLOAT, 'Deductible amount', VALUE_REQUIRED),
+                'usecredit' => new external_value(PARAM_INT, 'If we want to use the credit or not'),
+                'discount' => new external_value(PARAM_FLOAT, 'The sum of all discounts on the items.', VALUE_DEFAULT, 0),
+                'expirationtime' => new external_value(PARAM_INT, 'Expiration timestamp of cart', VALUE_REQUIRED),
+                'nowdate' => new external_value(PARAM_INT, 'current Timestamp', VALUE_REQUIRED),
+                'maxitems' => new external_value(PARAM_INT, 'Max Items', VALUE_REQUIRED),
+                'items' => new external_multiple_structure(
+                    new external_single_structure(
+                        [
+                            'userid' => new external_value(PARAM_INT, 'userid', VALUE_OPTIONAL),
+                            'itemid' => new external_value(PARAM_INT, 'Item id', VALUE_OPTIONAL),
+                            'itemname' => new external_value(PARAM_TEXT, 'Item name', VALUE_OPTIONAL),
+                            'price' => new external_value(PARAM_FLOAT, 'Price of item', VALUE_OPTIONAL),
+                            'price_gross' => new external_value(
+                                PARAM_FLOAT,
+                                'Gross price of item',
+                                VALUE_OPTIONAL
+                            ),
+                            'price_net' => new external_value(
+                                PARAM_FLOAT,
+                                'Net price of item',
+                                VALUE_OPTIONAL
+                            ),
+                            'tax' => new external_value(
+                                PARAM_FLOAT,
+                                'Net tax of item price',
+                                VALUE_OPTIONAL
+                            ),
+                            'taxcategory' => new external_value(
+                                PARAM_TAG,
+                                'Tax category of item',
+                                VALUE_OPTIONAL
+                            ),
+                            'taxpercentage' => new external_value(
+                                PARAM_FLOAT,
+                                'Tax percentage of item price as float',
+                                VALUE_OPTIONAL
+                            ),
+                            'taxpercentage_visual' => new external_value(
+                                PARAM_FLOAT,
+                                'Tax percentage of item price as an int',
+                                VALUE_OPTIONAL
+                            ),
+                            'currency' => new external_value(PARAM_ALPHA, 'Currency', VALUE_OPTIONAL),
+                            'componentname' => new external_value(
+                                PARAM_COMPONENT,
+                                'Component name',
+                                VALUE_OPTIONAL
+                            ),
+                            'costcenter' => new external_value(
+                                PARAM_TEXT,
+                                'Cost center for item',
+                                VALUE_OPTIONAL
+                            ),
+                            'area' => new external_value(
+                                PARAM_TEXT,
+                                'Area',
+                                VALUE_OPTIONAL
+                            ),
+                            'description' => new external_value(PARAM_RAW, 'Item description', VALUE_OPTIONAL),
+                            'imageurl' => new external_value(PARAM_RAW, 'Image url', VALUE_OPTIONAL),
+                            'canceluntil' => new external_value(
+                                PARAM_INT,
+                                'Timestamp until when cancel is possible',
+                                VALUE_OPTIONAL
+                            ),
+                            'nodelete' => new external_value(PARAM_INT, 'Marker for no delete', VALUE_OPTIONAL),
+                            'itemamount' => new external_value(PARAM_INT, 'Number of one item', VALUE_OPTIONAL),
 
-                                        ]
-                                )
-                        ),
-                ]
+                        ]
+                    )
+                ),
+            ]
         );
     }
 }
