b26daf6f13539f3772060b7f08c1bc7eadbc3ac9
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-09-05 10:01:59 +0200

    Version (2025090401)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
 $plugin->release = '1.0.17';
-$plugin->version = 2025090400;
+$plugin->version = 2025090401;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 405];
