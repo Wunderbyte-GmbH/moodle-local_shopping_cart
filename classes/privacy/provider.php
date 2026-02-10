e5ec45fe1cfe7dbc6b8d7f7a30abca587105d793
mm-poustini <mmp1368@gmail.com>
2026-02-10 11:37:28 +0100

    Improvement: Put the interface names in alphabetical order.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -46,8 +46,8 @@ use moodle_exception;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Get data from tables.
