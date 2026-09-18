29797a5d7d3880e507cc8342133850c2f61022ef
Georg Maisser <georg.maisser@wunderbyte.at>
2026-09-18 15:29:38 +0200

    Version 2.1.5 (2026091801)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_shopping_cart';
-$plugin->release = '2.1.3';
-$plugin->version = 2026081805;
+$plugin->release = '2.1.5';
+$plugin->version = 2026091801;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 502];
